e2e5f0788ab06e6b50e0a15f96476b2e5103607f
Ajinkya-B <ajinkya.bhosale85@gmail.com>
2023-12-01 23:41:37 -0500

    Edited home screen to match figma

--- React-Native/constants/images.ts
+++ React-Native/constants/images.ts
@@ -5,10 +5,10 @@ import qrCodeExample from '../assets/images/qr-code-example.png';
 import computerIcon from '../assets/icons/computer.svg';
 import help from '../assets/icons/help.svg';
 import lock from '../assets/icons/lock.svg';
-import redWarning from '../assets/iconsred_warning.svg';
+import redWarning from '../assets/icons/red_warning.svg';
 import settings from '../assets/icons/settings.svg';
 import shield from '../assets/icons/shield.svg';
-import timedLock from '../assets/icons/timedLock.svg';
+import timedLock from '../assets/icons/timed_lock.svg';
 import unlocked from '../assets/icons/unlocked.svg';
 import warning from '../assets/icons/warning.svg';
 
--- React-Native/pages/HomeScreen.tsx
+++ React-Native/pages/HomeScreen.tsx
@@ -6,6 +6,10 @@ import {images} from '../constants/images';
 function HomeScreen({navigation}: any): JSX.Element {
   return (
     <View style={styles.container}>
+      <View style={styles.header}>
+        <Image style={styles.largeImage} source={images.help} width={30} />
+        <Image style={styles.largeImage} source={images.settings} />
+      </View>
       <View style={styles.innerContainer}>
         <View style={styles.instructionContainer}>
           <Text style={styles.largeText}>
@@ -20,7 +24,7 @@ function HomeScreen({navigation}: any): JSX.Element {
         <View style={styles.buttonContainer}>
           <GenericButton
             text="Open camera"
-            width={160}
+            width={280}
             onPress={() => navigation.push('OpenCamera')}
           />
           <Text style={styles.linkText}>Didn't see a QR code?</Text>
@@ -34,7 +38,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#F7F3DF',
+    display: 'flex',
+    height: '100%',
+  },
+  header: {
+    backgroundColor: '#ECE2B2',
+    height: 60,
+    alignSelf: 'flex-start',
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
   },
   innerContainer: {
     width: '80%',
@@ -55,50 +69,25 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#00314B',
     textAlign: 'center',
+    marginBottom: '20%',
   },
   description: {
     fontSize: 16,
-    fontWeight: '500',
+    fontWeight: '400',
     color: '#00314B',
     textAlign: 'center',
+    marginBottom: 10,
   },
   linkText: {
+    marginTop: 10,
     fontWeight: '500',
     color: '#00314B',
     fontSize: 16,
-    textDecoration: 'underline',
-  },
-  dotNav: {
-    display: 'flex',
-    flexDirection: 'row',
-    marginTop: '10%',
-    width: '30%',
-    justifyContent: 'space-between',
-  },
-  dot: {
-    borderWidth: 2,
-    width: 20,
-    height: 20,
-    borderColor: '#00314B',
-    borderRadius: 50,
-  },
-  dotColored: {
-    borderWidth: 2,
-    width: 20,
-    height: 20,
-    borderColor: '#00314B',
-    borderRadius: 50,
-    backgroundColor: '#00314B',
+    textDecorationLine: 'underline',
   },
   buttonContainer: {
     display: 'flex',
     alignItems: 'center',
-  },
-  buttonNav: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    width: '100%',
-    flexDirection: 'row',
   },
 });
 
